98cd8ef05c0bc6b725d15357b62c5f1fddcbe0de
Gfy <Gfy@localhost>
2012-11-11 19:37:56 +0100

    new little script to delete nfo files inside srrs

---
+++ experiments/del_nfos.py
@@ -0,0 +1,14 @@
+# Deletes a lot of badly named nfo files
+
+import rescene
+
+srr = "Beauty.and.the.Beast.2012.S01E02.720p.HDTV.x264-IMMERSE.srr"
+good = "beauty.and.the.beast.2012.s01e02.720p.hdtv.x264-immerse.nfo"
+
+i = rescene.info(srr)
+i["stored_files"].pop(good)
+for sfile in  i["stored_files"].keys():
+	if sfile[-4:] != ".nfo":
+		i["stored_files"].pop(sfile)
+rescene.remove_stored_files(srr, i["stored_files"])
+print("Done!")
