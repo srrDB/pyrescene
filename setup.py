1617539ede18e5b5408ae62635288743a9442c38
Gfy <Gfy@localhost>
2013-02-05 15:38:44 +0100

    exe support

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from distutils.core import setup
+import rescene
+import sys
+
 # http://infinitemonkeycorps.net/docs/pph/
 # http://as.ynchrono.us/2007/12/filesystem-structure-of-python-project_21.html
 
@@ -15,20 +18,20 @@ from distutils.core import setup
 # build the package:
 # $ python setup.py sdist
 
-setup(
-    name = "pyReScene",
-    packages = ["rescene", "resample"],
-    scripts  = ["bin/srr", "bin/srs", "bin/pyrescene.py"],
-    version = "0.1",
-    description = "Python ReScene and ReSample implementation",
-    author = "Gfy", # ~umlaut@adsl-66-136-81-22.dsl.rcsntx.swbell.net (umlaut)
-    author_email = "gfy@lavabit.com",
-    url = "https://bitbucket.org/Gfy/pyrescene",
-    download_url = "https://bitbucket.org/Gfy/pyrescene/downloads",
-    license = "MIT",
-    keywords = ["rescene", "srr", "resample", "srs", "repackage", "rar",
+config_dict = {
+    "name": "pyReScene",
+    "packages": ["rescene", "resample"],
+    "scripts": ["bin/srr", "bin/srs", "bin/pyrescene.py"],
+    "version": rescene.__version__,
+    "description": "Python ReScene and ReSample implementation",
+    "author": "Gfy", # ~umlaut@adsl-66-136-81-22.dsl.rcsntx.swbell.net (umlaut)
+    "author_email": "gfy@lavabit.com",
+    "url": "https://bitbucket.org/Gfy/pyrescene",
+    "download_url": "https://bitbucket.org/Gfy/pyrescene/downloads",
+    "license": "MIT",
+    "keywords": ["rescene", "srr", "resample", "srs", "repackage", "rar",
 	            "avi", "mkv", "mp4", "wmv"],
-    classifiers = [
+    "classifiers": [
 		"Development Status :: 4 - Beta",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
@@ -41,7 +44,7 @@ setup(
         "Topic :: System :: Archiving :: Backup",
         "Topic :: Utilities"
         ], # http://pypi.python.org/pypi?:action=list_classifiers
-    long_description = """\
+    "long_description": """\
 pyReScene is a port of ReScene .NET to the Python programming language.
 ReScene is a mechanism for backing up and restoring the metadata from "scene" 
 released RAR files. RAR archive volumes are rebuild using the stored metadata 
@@ -49,4 +52,14 @@ in the SRR file and the extracted files from the RAR archive. Thus far this
 process only works on RAR files created with "Store" mode (otherwise known as 
 -m0 or No Compression). pyReScene consists of multiple related tools. 
 """
-)
+}
+
+# for creating Windows EXE files
+if 'py2exe' in sys.argv:
+	import py2exe
+	config_dict["options"] = {'py2exe': {'bundle_files': 1, 'compressed': True}}
+	config_dict["zipfile"] = None
+	# targets to build
+	config_dict["console"] = config_dict["scripts"]
+
+setup(**config_dict)
