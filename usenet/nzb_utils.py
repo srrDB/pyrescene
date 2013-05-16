c805d5f0bb085eff59b0da3b688b8b4aa3777232
Martin Panter <vadmium@localhost>
2013-05-17 09:35:02 +1000

    Handle ?basestring?, ?unicode? and ?long? type removal from Python 3
    * Check for numbers.Integral instead of long()

--- usenet/nzb_utils.py
+++ usenet/nzb_utils.py
@@ -120,7 +120,7 @@ on how to retrieve a part of a file.  Here's what you can find on an
 
 # fix pynzb library
 def _parse_date(date):
-	if isinstance(date, basestring):
+	if isinstance(date, pynzb.base.basestring):
 		date = int(date)
 	return datetime.datetime.fromtimestamp(date)
 pynzb.base.parse_date = _parse_date
