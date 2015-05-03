ee4e2865661cb0ab7b232239c8765ddac5ffaa33
Gfy <pyrescene@gmail.com>
2015-05-03 15:57:35 +0200

    Proper py2exe usage in setup.py and be able to create a specific .exe file

--- setup.py
+++ setup.py
@@ -25,8 +25,55 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 from distutils.core import setup
+from distutils.command.build_py import build_py
 import rescene
-import sys
+
+try:
+	import py2exe #@UnresolvedImport #@UnusedImport
+except:
+	pass
+
+class BuildFindUnrarExe(build_py):
+	"""Custom build command for unrar.exe build."""
+	description = "build exe that can locate unrar"
+
+	def run(self):
+		dist = self.distribution
+		dist.console = [{
+		    'script': "rescene/unrar.py",
+		    'icon_resources': [(1, 'images/icon.ico')]
+		},]
+		dist.command_options["py2exe"]["bundle_files"] = ('setup script', 1)
+		self.run_command('py2exe')
+		build_py.run(self)
+		
+class BuildSpecificExe(build_py):
+	"""Custom build command for specific EXE build."""
+	description = "build a specific pyReScene EXE file"
+	user_options = build_py.user_options + [
+		# The format is (long option, short option, description).
+		("path=", "p", "internal path to script e.g. 'rescene/unrar.py'"),
+	]
+	
+	def initialize_options(self, *args, **kwargs):
+		self.path = None
+		build_py.initialize_options(self, *args, **kwargs)
+
+	def run(self):
+		if not self.path:
+			print("Error: --path parameter must be provided")
+			print("       for example: -p \"usenet/srr_usenet.py\"")
+			return
+		dist = self.distribution
+		dist.console = [{
+		    'script': self.path,
+		    'icon_resources': [(1, 'images/icon.ico')]
+		},]
+		dist.command_options["py2exe"]["bundle_files"] = ('setup script', 1)
+		# enable to have one large .exe with everything:
+		# dist.zipfile = None
+		self.run_command('py2exe')
+		build_py.run(self)
 
 config_dict = {
     "name": "pyReScene",
@@ -68,34 +115,22 @@ pyReScene is a port of ReScene .NET to the Python programming language.
 ReScene is a mechanism for backing up and restoring the metadata from "scene" 
 released RAR files. RAR archive volumes are rebuild using the stored metadata 
 in the SRR file and the extracted files from the RAR archive. 
-pyReScene consists of multiple related tools. 
-"""
-}
+pyReScene consists of multiple related tools.
+""",
 
-def main():
-	# https://docs.python.org/3/distutils/sourcedist.html
-	# build the package:
-	# $ python setup.py sdist
-
-	# for creating Windows EXE files
-	if 'py2exe' in sys.argv:
-		import py2exe #@UnresolvedImport #@UnusedImport
-		
-		# http://www.py2exe.org/index.cgi/ListOfOptions
-		config_dict["options"] = {
-			'py2exe': {
+	# http://www.py2exe.org/index.cgi/ListOfOptions
+	"options": {
+		'py2exe': {
 			'bundle_files': 2,  # bundle everything but the Python interpreter
 			'optimize': 2,      # 2 = extra optimization (like python -OO)
 			'compressed': True  # compressed zipfile
-			}
 		}
-		
-		# name of shared zipfile to generate
-		# None: the files will be bundled within the executable
-		config_dict["zipfile"] = "pyrescenelibrary.zip"
-		
-		# targets to build
-		config_dict["console"] = [{
+	},
+	# name of shared zipfile to generate
+	# None: the files will be bundled within the executable
+	"zipfile": "pyrescenelibrary.zip",
+	# targets to build
+	"console": [{
 		    'script': "bin/srr.py",
 		    'icon_resources': [(1, 'images/icon.ico')]
 		},
@@ -114,8 +149,29 @@ def main():
 		{
 		    'script': "bin/retag.py",
 		    'icon_resources': [(1, 'images/icon.ico')]
-		}]
+		}
+	],
 			
+	"cmdclass": {
+		"unrar": BuildFindUnrarExe,
+		"exe": BuildSpecificExe,
+	}
+}
+
+def main():
+	"""
+	https://docs.python.org/3/distutils/sourcedist.html
+	
+	build the package:
+	$ python setup.py sdist
+	
+	build the executables:
+	$ python setup.py py2exe
+	
+	build a specific .exe file not in the build above
+	$ python setup.py exe --path "usenet/srr_usenet.py"
+	"""
+		
 	setup(**config_dict)
 
 if __name__ == '__main__':
@@ -133,3 +189,4 @@ if __name__ == '__main__':
 
 # http://www.pyinstaller.org/
 # http://sourceforge.net/projects/cx-freeze/
+# http://nuitka.net/pages/overview.html
