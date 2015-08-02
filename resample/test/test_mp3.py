c5e04fde3660eb2637074ba0f0ad19b1730059e1
Gfy <pyrescene@gmail.com>
2015-08-02 16:49:01 +0200

    MP3 test added

--- resample/test/test_mp3.py
+++ resample/test/test_mp3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2014 pyReScene
+# Copyright (c) 2014-2015 pyReScene
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
@@ -65,7 +65,7 @@ class TestDoubleId3v2(unittest.TestCase):
 		
 		# bad ID3 tag (10 bytes)
 		self.mp3stream.write(b"ID3")
-		self.mp3stream.write(b"N" * 7) # nfo data or other crap in file
+		self.mp3stream.write(b"N" * 7)  # nfo data or other crap in file
 		
 		# good ID3 tag (20 bytes)
 		writeId3v2Tag(self.mp3stream, 20)
@@ -121,6 +121,44 @@ class TestDoubleId3v2(unittest.TestCase):
 		offset = mp3.last_id3v2_before_sync(self.mp3stream, 0x10004)
 		self.assertEqual(10, offset)
 		
+class TestDoubleEndTags(unittest.TestCase):
+	"""MP3 files can become too large and the end data is repeated.
+	FTP ASCII/BIN transfer mix + file resume can result to this"""
+
+	def test_parse(self):
+		mp3stream = io.BytesIO()
+
+		# good ID3 tag (20 bytes)
+		writeId3v2Tag(mp3stream, 20)
+
+		# mp3 stuff (30 bytes)
+		writeMp3Data(mp3stream, 30)
+		
+		# repeating tags (with bad data inbetween) are a problem for retagging
+		writeId3v1Tag(mp3stream)
+		mp3stream.write(b"B" * 10)  # not necessary for test
+		writeId3v1Tag(mp3stream)
+		
+		# test if parsing succeeds
+		mr = mp3.Mp3Reader(stream=mp3stream)
+		generator = mr.read()
+		id3v2 = next(generator)
+		self.assertEqual(0, id3v2.start_pos)
+		self.assertEqual(20, id3v2.size)
+		
+		mp3data = next(generator)
+		self.assertEqual(20, mp3data.start_pos)
+		mp3_size = 30 + 128 + 10
+		self.assertEqual(mp3_size, mp3data.size)
+		# the crap is included in the mp3 data block
+		# it'll have to be cut off when retagging
+
+		id3v1 = next(generator)
+		self.assertEqual(20 + mp3_size, id3v1.start_pos)
+		self.assertEqual(128, id3v1.size)
+		
+		self.assertRaises(StopIteration, next, generator)
+
 class TestTags(unittest.TestCase):
 	def test_small_mp3(self):
 		mp3stream = io.BytesIO()
@@ -157,15 +195,15 @@ class TestTags(unittest.TestCase):
 		self.assertEqual(128, id3v1.size)
 		
 def writeId3v2Tag(stream, size=20):
-	assert size >= 10
+	assert size >= 10, "ID3v2 block must have a 10 byte header"
 	stream.write(b"ID3")
-	stream.write(b"\x03\x00\x00") # version info and flags
-	stream.write(mp3.encode_id3_size(size - 10)) # 10 byte header not included
-	stream.write(b"I" * (size - 10)) # data
+	stream.write(b"\x03\x00\x00")  # version info and flags
+	stream.write(mp3.encode_id3_size(size - 10))  # 10 byte header not included
+	stream.write(b"2" * (size - 10))  # data
 	
 def writeMp3Data(stream, size=20):
-	assert size >= 2
-	stream.write(b"\xFF\xE7") # mp3 frame marker (11 bits)
+	assert size >= 2, "The sync bytes require 2 bytes"
+	stream.write(b"\xFF\xE7")  # mp3 frame marker (11 bits)
 	stream.write(b"M" * (size - 2))
 	
 def writeId3v1Tag(stream):
