5e307bbc501041cce5335df24a53ef3c7ff88a5a
Gfy <pyrescene@gmail.com>
2016-02-15 22:25:37 +0100

    Bad .bat files fixed for use with the executables on Windows

--- bin/py2exe/auto.bat
+++ bin/py2exe/auto.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 REM pyReScene Auto is able to create .srr files from directories.
 REM This file uses all the suggested settings by default.
-python.exe %~dp0pyrescene.exe --best %*
+%~dp0pyrescene.exe --best %*
