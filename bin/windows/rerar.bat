35bbb922605bd86bb96cff4be6c7e08c807a0ac2
Gfy <Gfy@localhost>
2016-02-27 15:30:44 +0100

    rerar .bat file fixed

--- bin/windows/rerar.bat
+++ bin/windows/rerar.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 REM Try to recreate RARs based on RAR volume or .sfv file and trial/error.
-C:\Python34\python.exe %~dp0..\..\rerar\rerar.py %*
+REM Requires Python 3
+python.exe %~dp0..\..\rerar\rerar.py %*
