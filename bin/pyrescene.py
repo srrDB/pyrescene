c583582e86a6d752d40ebe98f69c9c9c309dc98c
Gfy <pyrescene@gmail.com>
2018-01-19 21:36:23 +0100

    Ignore fixed resolution movie posters that are showing up recently

--- bin/pyrescene.py
+++ bin/pyrescene.py
@@ -51,6 +51,8 @@ import fnmatch
 import time
 import logging
 import itertools
+import struct
+import imghdr
 
 try:
 	import win32api
@@ -282,8 +284,8 @@ def get_proof_files(reldir):
 							if grprls == grpimg:
 								similar_named = True
 								break
-					if similar_named:
-						result.append(proof)
+					if similar_named and not fixed_resolution_cover(proof):
+							result.append(proof)
 					else:
 						tpl = "'{0}' ({1} B) not added to SRR for release {2}"
 						msg = tpl.format(
@@ -321,6 +323,62 @@ def get_proof_files(reldir):
 						result.append(proof)
 						break
 	return result
+
+def fixed_resolution_cover(root_image):
+	"""Cut off movie poster image most likely added by a site script"""
+	try:
+		width, height = get_image_size(root_image)
+		return width == 630 and height == 1200
+	except TypeError:
+		return False;
+
+# image resolution check from https://stackoverflow.com/a/39778771/654160
+def test_jpeg(h, f):
+	# SOI APP2 + ICC_PROFILE
+	if h[0:4] == b"\xff\xd8\xff\xe2" and h[6:17] == b"ICC_PROFILE":
+		return "jpeg"
+	# SOI APP14 + Adobe
+	if h[0:4] == b"\xff\xd8\xff\xee" and h[6:11] == b"Adobe":
+		return "jpeg"
+	# SOI DQT
+	if h[0:4] == "\xff\xd8\xff\xdb":
+		return "jpeg"
+imghdr.tests.append(test_jpeg)
+
+def get_image_size(fname):
+	"""Determine the image type of fhandle and return its size. from draco"""
+	with open(fname, "rb") as fhandle:
+		head = fhandle.read(24)
+		if len(head) != 24:
+			return
+		what = imghdr.what(None, head)
+		if what == "png":
+			check = struct.unpack(">i", head[4:8])[0]
+			if check != 0x0d0a1a0a:
+				return
+			width, height = struct.unpack(">ii", head[16:24])
+		elif what == "gif":
+			width, height = struct.unpack("<HH", head[6:10])
+		elif what == "jpeg":
+			try:
+				fhandle.seek(0)  # Read 0xff next
+				size = 2
+				ftype = 0
+				while not 0xc0 <= ftype <= 0xcf or ftype in (0xc4, 0xc8, 0xcc):
+					fhandle.seek(size, 1)
+					byte = fhandle.read(1)
+					while ord(byte) == 0xff:
+						byte = fhandle.read(1)
+					ftype = ord(byte)
+					size = struct.unpack(">H", fhandle.read(2))[0] - 2
+				# We are at a SOFn block
+				fhandle.seek(1, 1)  # Skip `precision" byte.
+				height, width = struct.unpack(">HH", fhandle.read(4))
+			except Exception:  #IGNORE:W0703
+				return
+		else:
+			return
+		return width, height
 
 def remove_unwanted_sfvs(sfv_list, release_dir):
 	"""
