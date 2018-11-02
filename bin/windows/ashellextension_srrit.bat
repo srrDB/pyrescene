6cb17a3886e2a2512bba1e9664f61ae904eda7c6
Gfy <pyrescene@gmail.com>
2018-11-02 22:53:27 +0100

    Make a link to the bat in shell:SendTo able to handle multiple folders

--- bin/windows/ashellextension_srrit.bat
+++ bin/windows/ashellextension_srrit.bat
@@ -10,7 +10,7 @@ REM -v: an SRR file for vobsubs is included (install unrar or WinRAR)
 REM -o: sets the output path
 REM -r: can create .srr files recursively
 REM -y or -n: always yes or no for all prompts
-python.exe "%~dp0..\pyrescene.py" %1 -c -s -v -o %1\.. -r
+python.exe "%~dp0..\pyrescene.py" -c -s -v -o %1\.. -r %*
 
 REM Uncomment by removing REM to always keep the results on screen
 REM PAUSE
@@ -19,3 +19,4 @@ REM Keeps the results on screen when the process has errors
 IF %ERRORLEVEL% GTR 0 PAUSE
 
 REM Ctrl + C will cancel the SRR creation process (or Ctrl + Break)
+REM A link to this file can be added to the shell:SendTo menu
